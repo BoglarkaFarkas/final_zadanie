fc2ffd180ecc878a4c6beac584d25d8694907ed3
111481-GaborVarga <xvargag@stuba.sk>
2023-05-16 11:35:15 +0200

    List available tests for students + docker config update

--- src/logedStudent.php
+++ src/logedStudent.php
@@ -23,6 +23,45 @@ require_once('private/config.php');
         <li><a href="logedStudent.php" class="active"><img src = "photos/profile-circle-svgrepo-com.svg" alt="student"/></a></li>
         <li><a href="logout.php"><img src = "photos/log-out-svgrepo-com.svg" alt="logout"/></a></li>
     </ul>
-<?php echo "Hello " .$_SESSION["meno"]. " student"; ?>
+    <?php echo "Hello " .$_SESSION["meno"]. " student"; ?>
+    <h2>Dostupné testy pre vás</h2>
+
+  <?php
+  $sql = "SELECT * FROM testForStudents
+        WHERE ((startDate IS NOT NULL AND deadlineDate IS NOT NULL) AND (CONCAT(startDate, ' ', timeDate) <= NOW() AND CONCAT(deadlineDate, ' ', deadlineTime) >= NOW()))
+        OR (startDate IS NULL AND timeDate IS NULL AND deadlineDate IS NULL AND deadlineTime IS NULL)";
+
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $tests = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach ($tests as $row) {
+
+      echo count($tests) . "<br>";
+
+      $sql = "SELECT CONCAT(meno, ' ', priezvisko) AS name FROM myUserPanel WHERE id=?";
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute([$row['teacher_id']]);
+      $teacher = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      $sql = "SELECT latexSubor FROM zadanieLatex WHERE id=?";
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute([$row['latexSubor_id']]);
+      $file = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      echo "startDate: " . $row['startDate'] . "<br>";
+      echo "startTime: " . $row['timeDate'] . "<br>";
+      echo "deadlineDate: " . $row['deadlineDate'] . "<br>";
+      echo "deadlineTime: " . $row['deadlineTime'] . "<br>";
+      echo "file: " . $file['latexSubor'] . "<br>";
+      echo "teacher: " . $teacher['name'] . "<br>";
+      echo "max_points: " . $row['maxPoint'] . "<br>";
+      echo "<br>";
+  }
+
+
+
+  ?>
+
   </body>
 </html>
