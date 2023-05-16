a842c8ad346dd59c11923716ad39619104283276
PatrikNovak <xnovakp3@stuba.sk>
2023-05-16 21:45:25 +0200

    registration biling

--- src/private/components/registration.php
+++ src/private/components/registration.php
@@ -85,7 +85,7 @@
     <section class="py-5">
         <div class="container">
             <form action="" method="post">
-                <div class="title">Registrácia</div>
+                <div id="id9" class="title">Registrácia</div>
                 <div class="input-box underline fname">
                     <input type="text" id="first-name" name="first-name" placeholder="Zadaj krstné meno" required>
                     <div class="underline"></div>
@@ -100,11 +100,11 @@
                 </div>
                 <div class="input-box type form-floating">
                     <select class="form-select" name="type" id="floatingSelect" aria-label="Floating label">
-                        <option value="default" selected>Vyber si svoju rolu</option>
-                        <option value="student">Študent</option>
-                        <option value="ucitel">Učiteľ</option>
+                        <option id="id17" value="default" selected>Vyber si svoju rolu</option>
+                        <option id="id18" value="student">Študent</option>
+                        <option id="id19" value="ucitel">Učiteľ</option>
                     </select>
-                    <label for="floatingSelect">Rola</label>
+                    <label id="id16" for="floatingSelect">Rola</label>
                 </div>
                 <div class="input-box psw">
                     <input type="password" id="password" name="password" placeholder="Zadaj heslo" required>
@@ -115,9 +115,60 @@
                     <div class="underline"></div>
                 </div>
                 <div class="input-box button">
-                    <input type="submit" name="" value="Pokračuj">
+                    <input id="id15" type="submit" name="" value="Pokračuj">
                 </div>
+                <button id="sk-button">SK</button>
+                <button id="eng-button">ENG</button>
             </form>
         </div>
     </section>
+
+    <script>
+
+        document.getElementById("sk-button").addEventListener("click", function() {
+        fetch('../../bilingual.json')
+            .then(response => response.json())
+            .then(data => {
+            document.getElementById("id9").textContent = data.sk.id9;
+            document.getElementById("first-name").placeholder = data.sk.firstname;
+            document.getElementById("last-name").placeholder = data.sk.lastname;
+            document.getElementById("email").placeholder = data.sk.email;
+            document.getElementById("id17").textContent = data.sk.id17;
+            document.getElementById("id18").textContent = data.sk.id18;
+            document.getElementById("id19").textContent = data.sk.id19;
+            document.getElementById("id18").textContent = data.sk.id18;
+            document.getElementById("id16").textContent = data.sk.id16;
+            document.getElementById("password").placeholder = data.sk.password;
+            document.getElementById("confirm-password").placeholder = data.sk.confirmpassword;
+            document.getElementById("id15").textContent = data.sk.id15;
+            //document.getElementById("id5").textContent = data.sk.id5;
+            //document.getElementById("id6").textContent = data.sk.id6;
+            //document.getElementById("id7").innerHTML = data.sk.id7 + ' <a href="private/components/registration.php" id="id8">Zaregistrujte sa</a>';
+            // document.getElementById("id8").innerHTML = ' <a href="private/components/registration.php" id="id8">Zaregistrujte sa</a>';
+            });
+        });
+
+        document.getElementById("eng-button").addEventListener("click", function() {
+        fetch('../../bilingual.json')
+            .then(response => response.json())
+            .then(data => {
+            document.getElementById("id9").textContent = data.eng.id9;
+            document.getElementById("first-name").placeholder = data.eng.firstname;
+            document.getElementById("last-name").placeholder = data.eng.lastname;
+            document.getElementById("email").placeholder = data.eng.email;
+            document.getElementById("id17").textContent = data.eng.id17;
+            document.getElementById("id18").textContent = data.eng.id18;
+            document.getElementById("id19").textContent = data.eng.id19;
+            document.getElementById("id18").textContent = data.eng.id18;
+            document.getElementById("id16").textContent = data.eng.id16;
+            document.getElementById("password").placeholder = data.eng.password;
+            document.getElementById("confirm-password").placeholder = data.eng.confirmpassword;
+            document.getElementById("id15").textContent = data.eng.id15;
+            //document.getElementById("id5").textContent = data.eng.id5;
+            //document.getElementById("id6").textContent = data.eng.id6;
+            //document.getElementById("id7").innerHTML = data.eng.id7 + ' <a href="private/components/registration.php" id="id8">Sign up</a>';
+            //document.getElementById("id8").textContent = data.eng.id8;
+            });
+        });
+</script>
 </body>
