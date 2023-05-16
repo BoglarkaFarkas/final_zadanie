74b037ff6ef1637e0c259ae698a52f5cf8253de4
azuminka <peckovastanka@gmail.com>
2023-05-16 17:27:02 +0200

    db update

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,29 +1,27 @@
 USE db;
 
-CREATE TABLE IF NOT EXISTS myUserPanel (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  meno VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  priezvisko VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  email VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  heslo VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  role VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS zadanieLatex (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  latexSubor VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS testForStudents (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  startDate VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  timeDate VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  deadlineDate VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  deadlineTime VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_slovak_ci NOT NULL,
-  latexSubor_id INT NOT NULL,
-  teacher_id INT NOT NULL,
-  maxPoint INT NOT NULL
-);
-
-INSERT IGNORE INTO myUserPanel (id, meno, priezvisko, email, heslo, role)
-VALUES (1,'Olga', 'Kohut', 'kohut9@stuba.sk', '$argon2id$v=19$m=65536,t=4,p=1$OVpBMEE4cXJjcTVENFZWRQ$AtLSBxR3m230vZ3u/gyZviZV+KHhJk1OS5b5LV+AYC8', 'ucitel');
+CREATE TABLE IF NOT EXISTS examples_ (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT , 
+	file_name VARCHAR(100) NOT NULL , 
+	example_name VARCHAR(50) NOT NULL , 
+	example_body TEXT NOT NULL , 
+	solution TEXT NOT NULL , 
+	start_date DATE NULL DEFAULT NULL , 
+	deadline_date DATE NULL DEFAULT NULL , 
+	points TINYINT UNSIGNED NOT NULL , 
+	solvable BOOLEAN NOT NULL , 
+	PRIMARY KEY (id)
+) ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS users (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT , 
+	name VARCHAR(100) NOT NULL , 
+	surname VARCHAR(50) NOT NULL , 
+	email VARCHAR(50) NOT NULL , 
+	password VARCHAR(100) NOT NULL , 
+	role VARCHAR(10) NOT NULL , 
+	PRIMARY KEY (id)
+) ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS studentTest (
+	id_student INT UNSIGNED NOT NULL , 
+	id_example INT UNSIGNED NOT NULL 
+) ENGINE = InnoDB;
